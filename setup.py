51ee1c840547a58cb1d14822d36466a5eb736543
denfed <dcfedori@buffalo.edu>
2024-05-15 14:11:26 -0400

    changed setup

--- setup.py
+++ setup.py
@@ -27,18 +27,18 @@ import subprocess
 import subprocess
 import sys
 
-def install_torch():
-    try:
-        import torch
-    except ImportError:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "torch"])
+# def install_torch():
+#     try:
+#         import torch
+#     except ImportError:
+#         subprocess.check_call([sys.executable, "-m", "pip", "install", "torch"])
 
-# Call the function to ensure torch is installed
-install_torch()
+# # Call the function to ensure torch is installed
+# install_torch()
 
-import torch
+# import torch
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
+# from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
 # groundingdino version info
 version = "0.1.0"
@@ -60,59 +60,59 @@ def write_version_file():
         # f.write(f"git_version = {repr(sha)}\n")
 
 
-requirements = ["torch", "torchvision"]
+# requirements = ["torch", "torchvision"]
 
-torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
+# torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
 
 
-def get_extensions():
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    extensions_dir = os.path.join(this_dir, "groundingdino", "models", "GroundingDINO", "csrc")
+# def get_extensions():
+#     this_dir = os.path.dirname(os.path.abspath(__file__))
+#     extensions_dir = os.path.join(this_dir, "groundingdino", "models", "GroundingDINO", "csrc")
 
-    main_source = os.path.join(extensions_dir, "vision.cpp")
-    sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"))
-    source_cuda = glob.glob(os.path.join(extensions_dir, "**", "*.cu")) + glob.glob(
-        os.path.join(extensions_dir, "*.cu")
-    )
+#     main_source = os.path.join(extensions_dir, "vision.cpp")
+#     sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"))
+#     source_cuda = glob.glob(os.path.join(extensions_dir, "**", "*.cu")) + glob.glob(
+#         os.path.join(extensions_dir, "*.cu")
+#     )
 
-    sources = [main_source] + sources
+#     sources = [main_source] + sources
 
-    extension = CppExtension
+#     extension = CppExtension
 
-    extra_compile_args = {"cxx": []}
-    define_macros = []
+#     extra_compile_args = {"cxx": []}
+#     define_macros = []
 
-    if CUDA_HOME is not None and (torch.cuda.is_available() or "TORCH_CUDA_ARCH_LIST" in os.environ):
-        print("Compiling with CUDA")
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [("WITH_CUDA", None)]
-        extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ]
-    else:
-        print("Compiling without CUDA")
-        define_macros += [("WITH_HIP", None)]
-        extra_compile_args["nvcc"] = []
-        return None
+#     if CUDA_HOME is not None and (torch.cuda.is_available() or "TORCH_CUDA_ARCH_LIST" in os.environ):
+#         print("Compiling with CUDA")
+#         extension = CUDAExtension
+#         sources += source_cuda
+#         define_macros += [("WITH_CUDA", None)]
+#         extra_compile_args["nvcc"] = [
+#             "-DCUDA_HAS_FP16=1",
+#             "-D__CUDA_NO_HALF_OPERATORS__",
+#             "-D__CUDA_NO_HALF_CONVERSIONS__",
+#             "-D__CUDA_NO_HALF2_OPERATORS__",
+#         ]
+#     else:
+#         print("Compiling without CUDA")
+#         define_macros += [("WITH_HIP", None)]
+#         extra_compile_args["nvcc"] = []
+#         return None
 
-    sources = [os.path.join(extensions_dir, s) for s in sources]
-    include_dirs = [extensions_dir]
+#     sources = [os.path.join(extensions_dir, s) for s in sources]
+#     include_dirs = [extensions_dir]
 
-    ext_modules = [
-        extension(
-            "groundingdino._C",
-            sources,
-            include_dirs=include_dirs,
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
+#     ext_modules = [
+#         extension(
+#             "groundingdino._C",
+#             sources,
+#             include_dirs=include_dirs,
+#             define_macros=define_macros,
+#             extra_compile_args=extra_compile_args,
+#         )
+#     ]
 
-    return ext_modules
+#     return ext_modules
 
 
 def parse_requirements(fname="requirements.txt", with_version=True):
@@ -215,6 +215,6 @@ if __name__ == "__main__":
                 "tests",
             )
         ),
-        ext_modules=get_extensions(),
-        cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+        # ext_modules=get_extensions(),
+        # cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
     )
